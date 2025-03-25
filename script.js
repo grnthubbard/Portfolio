cba7a9fe66be57c11e9375a0647ff530589a48fd
Grant Hubbard <gdhubbar@purdue.edu>
2025-03-25 13:20:08 -0400

    Main page done, need to add projects still

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-var typed = new Typed('#typed', {
-    strings: ["Hello", "I'm Grant Hubbard."],
-    typeSpeed: 30
+document.addEventListener("DOMContentLoaded", function(){
+  var typed = new Typed("#typed-output", {
+    strings: ["Hello, I'm Grant Hubbard.", "Let's Build Something Great!", "Hello, I'm Grant Hubbard."],
+    typeSpeed: 50,
+    backSpeed: 30,
+    startDelay: 2000,
+    backDelay: 2000,
+    loop: false,
+    showCursor: true,
+    cursorChar: "|",
   });
+});
